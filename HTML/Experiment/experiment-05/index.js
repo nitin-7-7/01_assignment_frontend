81ae4bfd1b2f5e0c02c2c5a2291c26d8e14dca07
nitin-7-7 <nitincatt@gmail.com>
2025-10-29 11:11:26 +0530

    experiment

---
+++ HTML/Experiment/experiment-05/index.js
@@ -0,0 +1,15 @@
+// task 1:
+
+// alert("Welcome to meri falstu website")
+// const name = prompt("naam dalee apnaa!!")
+// alert("kese ho bhai "+ name +"!")
+// document.write("<h2>Hello " + name + "! KEM CHHO.</h2>");
+
+// task 2:
+
+const age = prompt("AGE BTAA !")
+if(age>18){
+    alert("votes deshkta hai bhai tu ")
+}else{
+    alert("jake POGO dekh!")
+}
